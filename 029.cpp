6b1438a5efb8c093abc46eb199d6b49ab939365a
maxioul <maxiou7391468250@126.com>
2020-10-04 13:19:00 +0900

    Update 029.cpp

--- 029.cpp
+++ 029.cpp
@@ -1,9 +1,37 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if(divisor==-1&&dividend==INT_MIN){
-            return INT_MAX;
+        if(dividend==INT_MIN){
+            if(divisor==-1)return INT_MAX;
+            if(divisor==1)return INT_MIN;
         }
-        return dividend/divisor;
+        bool sign = true;
+        if(dividend<0){
+            dividend = abs(dividend);
+            sign = !sign;
+        }
+        if(divisor<0){
+            divisor = abs(divisor);
+            sign = !sign;
+        }
+        long int did = labs(dividend), dir = labs(divisor);
+        int mul = 1;
+        int output = 0;
+        bool hit_max = true;
+        while(mul>0){
+            if(did>=dir){
+                did-=dir;
+                output+=mul;
+                if(hit_max&&did>=dir){
+                    dir<<=1;
+                    mul<<=1;
+                }
+            }else{
+                hit_max = false;
+                dir>>=1;
+                mul>>=1;
+            }
+        }
+        return sign?output:-1*output;
     }
 };
