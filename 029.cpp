1ef4f8f1608bb9abf3e929362c491c2742d588ea
maxioul <maxiou7391468250@126.com>
2020-10-04 12:36:28 +0900

    Update 029.cpp

--- 029.cpp
+++ 029.cpp
@@ -1,23 +1,9 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        long long a = dividend;
-        long long b = divisor;
-        long long Max,Min;
-        if(b>0){
-            Max = INT_MAX*b;
-            Min = INT_MIN*b;
-        }
-        if(b<0){
-            Min = INT_MAX*b;
-            Max = INT_MIN*b;
-        }
-        if(dividend>Max){
+        if(divisor==-1&&dividend==INT_MIN){
             return INT_MAX;
         }
-        if(dividend<Min){
-            return INT_MAX;
-        }
-        return dividend/b;
+        return dividend/divisor;
     }
 };
